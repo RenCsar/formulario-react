a205db12a96489d1e5d9db6a0b49c437e8157d51
Renan Cesar <cesar.renan@hotmail.com>
2022-11-07 12:52:56 -0300

    Finalizando estilização do formulário

---
+++ src/Form/index.jsx
@@ -0,0 +1,39 @@
+import styles from './Form.module.css';
+import { useForm } from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers';
+import * as yup from 'yup';
+
+
+export default function Form(){
+    return(
+        <>
+            <section className={styles.formulario}>
+                <div className={styles.formContainer}>
+                    <form action="#">
+                        <div className={styles.tituloForm}>
+                            <h3>Deixe sua opinião</h3>
+                        </div>
+                        <div className={styles.fild}>
+                            <label className={styles.label}>Nome:</label>
+                            <input type="text" placeholder='Digite seu nome' />
+                            <small>erros</small>
+                        </div>
+                        <div className={styles.fild}>
+                            <label className={styles.label}>Email:</label>
+                            <input type="email" placeholder='Digite seu e-mail' />
+                            <small>erros</small>
+                        </div>
+                        <div className={styles.coment}>
+                            <label className={styles.label}>Opinião:</label>
+                            <textarea placeholder='Digi sua opinião aqui'/>
+                            <small>erros</small>
+                        </div>
+                        <div className={styles.btnContainer}>
+                            <button type='submit'>Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </>
+    )
+}
