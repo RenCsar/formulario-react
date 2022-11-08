9b774e78aba49fda78929c0a98128e8898df7ba9
Renan Cesar <cesar.renan@hotmail.com>
2022-11-08 12:35:00 -0300

    Adicionando modal

--- src/Form/index.jsx
+++ src/Form/index.jsx
@@ -2,12 +2,13 @@ import styles from './Form.module.css';
 import { useForm } from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import {useState} from 'react';
 
 
 export default function Form(){
 
     const schema = yup.object({
-        name: yup.string().required("Preencha esse campo"),
+        name: yup.string().required("Preencha esse campo").min(3, "Você precisar inserir pelo menos 3 caracteres"),
         email: yup.string().email("Formato de email inválido").required("Use um email válido"),
         suggestion: yup.string().required("Não deixe esse campo vazio")
     });
@@ -15,18 +16,28 @@ export default function Form(){
 
     const {register, handleSubmit, formState: { errors}} = useForm({
         resolver: yupResolver(schema)
-    })
+    });
 
     const newSugestion = (data)=>{
         console.log(data);
-        alert("Sua sugestão foi recebida");
+        setModalState(false);
+    };
+    
+    const [modalState, setModalState] = useState(true);
+
+    let hide = modalState ? styles.hide : styles.show;
+    let show = modalState ? styles.show : styles.hide;
+    
+    let fecharModal = () => {
+        setModalState(true);
         window.location.reload();
-    }
+    };
+
 
     return(
         <>
             <section className={styles.formulario}>
-                <div className={styles.formContainer}>
+                <div className={`${styles.formContainer} ${show}`}>
                     <form onSubmit={handleSubmit(newSugestion)}>
                         <div className={styles.tituloForm}>
                             <h3>Deixe sua opinião</h3>
@@ -47,11 +58,17 @@ export default function Form(){
                             <small>{errors.suggestion?.message}</small>
                         </div>
                         <div className={styles.btnContainer}>
-                            <button type='submit'>Enviar</button>
+                            <button type='submit' >Enviar</button>
                         </div>
                     </form>
                 </div>
+                <div className={`${styles.modalContainer} ${hide}`}>
+                    <h2>Sugestão enviada</h2>
+                    <div className={styles.btnContainer}>
+                            <button type='submit'onClick={()=> fecharModal()}>OK</button>
+                    </div>
+                </div>
             </section>
         </>
-    )
-}
+    );
+};
